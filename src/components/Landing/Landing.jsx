c968a10bc7d237a6af32155dc3ed2b772cf8d6b2
mikhail <klyushinmisha@gmail.com>
2019-11-09 18:11:28 +0300

    add landing component

---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import Image from '../Image/Image.jsx';
+import home from '../../assets/images/landing/home.png';
+
+export default function Landing() {
+    return <Image src={home} maxHeight={100}/>
+}
