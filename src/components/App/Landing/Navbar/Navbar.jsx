de235c4377e012f2fcc22121e2a8dcaf9e1fec41
mikhail <klyushinmisha@gmail.com>
2019-11-11 08:16:27 +0300

    add sticky top class

--- src/components/App/Landing/Navbar/Navbar.jsx
+++ src/components/App/Landing/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import Logo from './Logo/Logo.jsx';
 import LoginNav from './LoginNav/LoginNav';
 
 export default function Navbar() {
-    return <nav className='navbar navbar-dark navbar-expand-lg bg-light'>
+    return <nav className='navbar sticky-top navbar-dark navbar-expand-lg bg-light'>
         <a className='navbar-brand' href='/'>
             <Logo maxHeight='5vh'/>
         </a>
