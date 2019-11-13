bc51f21847ccb5819fee811d4599d3148aea008a
mikhail <klyushinmisha@gmail.com>
2019-11-13 23:35:08 +0300

    add floating navbar

--- src/components/App/Landing/Navbar/Navbar.jsx
+++ src/components/App/Landing/Navbar/Navbar.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './Navbar.scss';
 import Logo from './Logo/Logo.jsx';
 import LoginNav from './LoginNav/LoginNav';
 
 export default function Navbar() {
-    return <nav className='navbar sticky-top navbar-dark navbar-expand-lg bg-light'>
+    const [isHidden, setIsHidden] = useState(false);
+
+    useEffect(() => {
+        let prevPos = window.scrollY;
+        const handleScroll = () => {
+            setIsHidden(window.scrollY > prevPos);
+            prevPos = window.scrollY;
+        }
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, [setIsHidden]);
+
+    let classes = [
+        'navbar',
+        'sticky-top',
+        'navbar-dark',
+        'navbar-expand-lg',
+        'bg-light'
+    ];
+    if (isHidden) {
+        classes.push('hidden');
+    }
+
+    return <nav className={classes.join(' ')}>
         <a className='navbar-brand' href='/'>
             <Logo maxHeight='5vh'/>
         </a>
